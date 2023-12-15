fdd96ab955e5f9cc677de794430d56cc2008a4e5
VOB <vobc@live.com>
2023-12-15 16:41:50 +0100

    day9part1 solution

--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day9 {
@@ -9,11 +11,41 @@ public class Day9 {
         input = textParser.lines("src/main/Day9");
     }
 
-    public int part1() {
-        return 0;
+    public Day9(String path) {
+        input = textParser.lines(path);
     }
 
-    public int part2() {
+    public long part1() {
+        long extrapolatedValueSum = 0;
+        for (String line : input) {
+            extrapolatedValueSum += extrapolateHistory(line);
+        }
+
+        return extrapolatedValueSum;
+    }
+
+    public long extrapolateHistory(String line) {
+        List<Long> history = Arrays.stream(line.split(" ")).map(Long::parseLong).toList();
+
+        return findNextHistoryValue(history);
+    }
+
+    private long findNextHistoryValue(List<Long> history) {
+        List<Long> interpolationValues = new ArrayList<>();
+
+        for (int i=1; i<history.size(); i++) {
+            interpolationValues.add(history.get(i) - history.get(i-1));
+        }
+
+        List<Long> filterZeroes = interpolationValues.stream().filter(i -> i != 0).toList();
+        if (filterZeroes.size() != 0) {
+            return history.get(history.size()-1) + findNextHistoryValue(interpolationValues);
+        } else {
+            return history.get(history.size()-1);
+        }
+    }
+
+    public long part2() {
         return 0;
     }
 }
--- src/test/java/Day9Test.java
+++ src/test/java/Day9Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day9Test {
@@ -7,6 +8,18 @@ public class Day9Test {
     @Test
     public void Day9Part1() {
         System.out.println(day9.part1());
+    }
+
+    @Test
+    public void Day9Part1Test() {
+        Day9 day9Test = new Day9("src/main/Day9Test");
+        Assertions.assertEquals(114, day9Test.part1());
+    }
+
+    @Test
+    public void extrapolateHistoryTest() {
+        Assertions.assertEquals(18, day9.extrapolateHistory("0 3 6 9 12 15"));
+        Assertions.assertEquals(28, day9.extrapolateHistory("1 3 6 10 15 21"));
     }
 
     @Test
