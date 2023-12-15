e5fdc8152080785c5a15d57a3b2f28c51d3bc9ac
VOB <vobc@live.com>
2023-12-15 22:53:27 +0100

    day11part1 solution

--- src/main/java/Day11.java
+++ src/main/java/Day11.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day11 {
@@ -7,10 +9,72 @@ public class Day11 {
 
     public Day11() {
         input = textParser.lines("src/main/Day11");
+        expandGalaxy();
+    }
+
+    public Day11(String path) {
+        input = textParser.lines(path);
+        expandGalaxy();
+    }
+
+    private void expandGalaxy() {
+        for (int i=input.size()-1; i>=0; i--) {
+            if (!input.get(i).contains("#")) {
+                input.add(i, input.get(i));
+            }
+        }
+        for (int i=input.get(0).length()-1; i>=0; i--) {
+            addColumnsOfSpace(i);
+        }
+    }
+
+    private void addColumnsOfSpace(int column) {
+        for (String s : input) {
+            if (s.charAt(column) == '#') {
+                return;
+            }
+        }
+        List<String> newInput = new ArrayList<>();
+        for (String s : input) {
+            newInput.add(s.substring(0, column) + '.' + s.substring(column));
+        }
+        input = newInput;
     }
 
     public int part1() {
-        return 0;
+        List<Point> galaxyLocations = new ArrayList<>();
+        for (int i=0; i<input.size(); i++) {
+            for (int j=0; j<input.get(0).length(); j++) {
+                if (input.get(i).charAt(j) == '#') {
+                    galaxyLocations.add(new Point(i,j));
+                }
+            }
+        }
+
+        int sumOfLengths = 0;
+        for (int i=0; i<galaxyLocations.size(); i++) {
+            Point current = galaxyLocations.get(i);
+            for (int j=i+1; j<galaxyLocations.size(); j++) {
+                Point other = galaxyLocations.get(j);
+                sumOfLengths += distanceBetweenGalaxies(current, other);
+            }
+        }
+        return sumOfLengths;
+    }
+
+    private int distanceBetweenGalaxies(Point current, Point other) {
+        int distance = 0;
+        if (current.x < other.x) {
+            distance = other.x - current.x;
+        } else {
+            distance = current.x - other.x;
+        }
+        if (current.y < other.y) {
+            distance += other.y - current.y;
+        } else {
+            distance += current.y - other.y;
+        }
+        return distance;
     }
 
     public int part2() {
--- src/test/java/Day11Test.java
+++ src/test/java/Day11Test.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day11Test {
 
@@ -6,11 +10,36 @@ public class Day11Test {
 
     @Test
     public void Day11Part1() {
-        System.out.println(day11.part1());
+        Assertions.assertEquals(9599070, day11.part1());
     }
 
     @Test
     public void Day11Part2() {
         System.out.println(day11.part2());
+    }
+
+    @Test
+    public void assertThatGalaxyExpandsCorrectly() {
+        Day11 day11Test = new Day11("src/main/Day11Test");
+        List<String> expectedGalaxy = new ArrayList<>();
+        expectedGalaxy.add("....#........");
+        expectedGalaxy.add(".........#...");
+        expectedGalaxy.add("#............");
+        expectedGalaxy.add(".............");
+        expectedGalaxy.add(".............");
+        expectedGalaxy.add("........#....");
+        expectedGalaxy.add(".#...........");
+        expectedGalaxy.add("............#");
+        expectedGalaxy.add(".............");
+        expectedGalaxy.add(".............");
+        expectedGalaxy.add(".........#...");
+        expectedGalaxy.add("#....#.......");
+        Assertions.assertEquals(expectedGalaxy, day11Test.input);
+    }
+
+    @Test
+    public void Day11Part1Test() {
+        Day11 day11Test = new Day11("src/main/Day11Test");
+        Assertions.assertEquals(374, day11Test.part1());
     }
 }
