d9effde87074a98e00710898b3588b316d024354
VOB <vobc@live.com>
2023-12-18 16:37:55 +0100

    day10part1 solution

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -1,11 +1,13 @@
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class Day10 {
 
     TextParser textParser = new TextParser();
     char[][] input;
 
-    Map<Character, List<Character>> shapes = new HashMap<>();
+    Map<Character, List<String>> shapes = new HashMap<>();
 
     public Day10() {
         List<String> lines = textParser.lines("src/main/Day10");
@@ -17,8 +19,18 @@ public class Day10 {
         addShapes();
     }
 
+    public Day10(String path) {
+        List<String> lines = textParser.lines(path);
+        input = new char[lines.get(0).length()][lines.size()];
+        for (int i=0; i<lines.size(); i++) {
+            input[i] = lines.get(i).toCharArray();
+        }
+
+        addShapes();
+    }
+
     public int part1() {
-        List<List<Integer>> positions = collectPositions(input);
+        List<Point> positions = collectPositions(input);
 
         return (int) (positions.size()/2D);
     }
@@ -27,49 +39,94 @@ public class Day10 {
         return 0;
     }
 
-    private List<List<Integer>> collectPositions(char[][] input) {
-        List<Integer> startingPosition = findStartingPosition(input);
-        List<List<Integer>> positions = findInitialDirection(startingPosition);
-        positions = addNextPosition(List.of(startingPosition), startingPosition);
+    protected List<Point> collectPositions(char[][] input) {
+        Point startingPosition = findStartingPosition(input);
+        List<Point> positions = findInitialDirection(startingPosition);
+        positions = traverseLoop(positions);
 
         return positions;
     }
 
-    private List<List<Integer>> findInitialDirection(List<Integer> startingPosition) {
-        if (startingPosition.get(0) < input.length) {
-            char southCharacter = input[startingPosition.get(0)][startingPosition.get(1)];
+    private List<Point> findInitialDirection(Point startingPosition) {
+        if (startingPosition.x < input.length) {
+            char southCharacter = input[startingPosition.x+1][startingPosition.y];
             if (southCharacter == '|' || southCharacter == 'L' || southCharacter == 'J') {
-                return List.of(startingPosition, List.of(startingPosition.get(0), startingPosition.get(1)));
+                return List.of(startingPosition, new Point(startingPosition.x+1, startingPosition.y));
             }
         }
+        if (startingPosition.x > 0) {
+            char northCharacter = input[startingPosition.x-1][startingPosition.y];
+            if (northCharacter == '|' || northCharacter == '7' || northCharacter == 'F') {
+                return List.of(startingPosition, new Point(startingPosition.x+1, startingPosition.y));
+            }
+        }
+
+        if (startingPosition.y < input[startingPosition.x].length) {
+            char eastCharacter = input[startingPosition.x][startingPosition.y+1];
+            if (eastCharacter == '-' || eastCharacter == 'J' ||  eastCharacter == '7') {
+                return List.of(startingPosition, new Point(startingPosition.x, startingPosition.y+1));
+            }
+        }
+
+        //Assumption that there exists a loop, therefore no need to check 4th direction
 
         return List.of(startingPosition);
     }
 
-    private List<List<Integer>> addNextPosition(List<List<Integer>> positions, List<Integer> previous) {
-        List<Integer> startingPosition = positions.get(0);
+    private List<Point> traverseLoop(List<Point> inputList) {
+        List<Point> positions = new ArrayList<>(inputList);
 
-        return positions;
+        while (true) {
+            Point currentPoint = positions.get(positions.size()-1);
+            Point previousPoint = positions.get(positions.size()-2);
+            char currentCharacter = input[currentPoint.x][currentPoint.y];
+
+            List<String> directions = new ArrayList<>(shapes.get(currentCharacter));
+
+            if (currentPoint.x < previousPoint.x) {
+                directions.remove("s");
+            }
+            if (currentPoint.x > previousPoint.x) {
+                directions.remove("n");
+            }
+            if (currentPoint.y < previousPoint.y) {
+                directions.remove("e");
+            }
+            if (currentPoint.y > previousPoint.y) {
+                directions.remove("w");
+            }
+
+            switch (directions.get(0)) {
+                case "n" -> positions.add(new Point(currentPoint.x-1, currentPoint.y));
+                case "s" -> positions.add(new Point(currentPoint.x+1, currentPoint.y));
+                case "e" -> positions.add(new Point(currentPoint.x, currentPoint.y+1));
+                case "w" -> positions.add(new Point(currentPoint.x, currentPoint.y-1));
+            }
+
+            if (input[positions.get(positions.size()-1).x][positions.get(positions.size()-1).y] == 'S') {
+                return positions;
+            }
+        }
     }
 
-    private List<Integer> findStartingPosition(char[][] input) {
+    private Point findStartingPosition(char[][] input) {
         for (int i=0; i<input.length;i++) {
             for (int j=0; j<input[i].length; j++){
                 if (input[i][j] == 'S'){
-                    return List.of(i, j);
+                    return new Point(i,j);
                 }
             }
         }
         //No S present in input
-        return List.of();
+        return new Point(-1,-1);
     }
 
     private void addShapes() {
-        shapes.put('|', List.of('n', 's'));
-        shapes.put('-', List.of('e', 'w'));
-        shapes.put('L', List.of('n', 'e'));
-        shapes.put('J', List.of('n', 'w'));
-        shapes.put('7', List.of('s', 'w'));
-        shapes.put('F', List.of('s', 'e'));
+        shapes.put('|', List.of("n", "s"));
+        shapes.put('-', List.of("e", "w"));
+        shapes.put('L', List.of("n", "e"));
+        shapes.put('J', List.of("n", "w"));
+        shapes.put('7', List.of("s", "w"));
+        shapes.put('F', List.of("s", "e"));
     }
 }
--- src/test/java/Day10Test.java
+++ src/test/java/Day10Test.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.List;
 
 public class Day10Test {
 
@@ -12,5 +16,23 @@ public class Day10Test {
     @Test
     public void Day10Part2() {
         System.out.println(day10.part2());
+    }
+
+    @Test
+    public void Day10Part1Test() {
+        Day10 day10Test = new Day10("src/main/Day10Test");
+        System.out.println(day10Test.part1());
+    }
+
+    @Test
+    public void verifyTraversal() {
+        Day10 day10Test = new Day10("src/main/Day10Test");
+        String path = "S|L-J|7-S";
+        StringBuilder actualPath = new StringBuilder();
+        List<Point> points = day10Test.collectPositions(day10Test.input);
+        for(Point point : points) {
+            actualPath.append(day10Test.input[point.x][point.y]);
+        }
+        Assertions.assertEquals(path, actualPath.toString());
     }
 }
