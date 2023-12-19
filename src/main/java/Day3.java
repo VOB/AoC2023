df2d3b6d7b65a8cc7414a59c6a67adbde71a0bb2
VOB <vobc@live.com>
2023-12-19 19:44:04 +0100

    day3part2 solution

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +11,7 @@ public class Day3 {
     List<String> input;
     int inputWidth;
     int inputHeight;
+    Map<Point, List<Integer>> gears = new HashMap<>();
 
     public Day3() {
         input = textParser.lines("src/main/Day3");
@@ -16,8 +19,8 @@ public class Day3 {
         inputHeight = input.size();
     }
 
-    public Day3(List<String> input) {
-        this.input = input;
+    public Day3(String path) {
+        this.input = textParser.lines(path);
         inputWidth = input.get(0).length();
         inputHeight = input.size();
     }
@@ -39,58 +42,121 @@ public class Day3 {
         return sum;
     }
 
+    public int part2() {
+        int sum = 0;
+
+        for (int xCoord = 0; xCoord < inputHeight; xCoord++) {
+            String line = input.get(xCoord);
+            for (int yCoord = 0; yCoord < line.length(); yCoord++) {
+                if (yCoord != 0 && Character.isDigit(line.charAt(yCoord-1))) {
+                    //do nothing.
+                }
+                else if (Character.isDigit(line.charAt(yCoord))) {
+                    partNumber(xCoord, yCoord);
+                }
+            }
+        }
+
+        for (List<Integer> partsWithGears : gears.values()) {
+            if (partsWithGears.size() == 2) {
+                sum += (partsWithGears.get(0) * partsWithGears.get(1));
+            }
+        }
+        return sum;
+    }
+
+
     public int partNumber(int xCoord, int yCoord) {
         StringBuilder partNumberAsString = new StringBuilder();
-        StringBuilder surroundingSymbols = new StringBuilder();
+        List<Point> numberCoordinates = new ArrayList<>();
 
         int offset = 0;
         while (yCoord+offset < inputWidth && Character.isDigit(input.get(xCoord).charAt(yCoord+offset))) {
             partNumberAsString.append(input.get(xCoord).charAt(yCoord + offset));
-            surroundingSymbols.append(getSurroundingSymbols(xCoord, yCoord + offset));
+            numberCoordinates.add(new Point(xCoord, yCoord+offset));
             offset++;
         }
 
-        if (!hasSymbols(surroundingSymbols.toString())) {
+        int partNumber = Integer.parseInt(partNumberAsString.toString());
+        String surroundingSymbols = getSurroundingSymbols(numberCoordinates, partNumber);
+        if (!hasSymbols(surroundingSymbols)) {
             return 0;
         }
 
-        return Integer.parseInt(partNumberAsString.toString());
+        return partNumber;
     }
 
-    public String getSurroundingSymbols(int xCoord, int yCoord) {
+    public String getSurroundingSymbols(List<Point> numberCoordinates, int partNumber) {
+        int xCoord = numberCoordinates.get(0).x;
+        int yCoordMin = numberCoordinates.get(0).y;
+        int yCoordMax = numberCoordinates.get(numberCoordinates.size()-1).y;
         StringBuilder surroundingSymbols = new StringBuilder();
 
-        if (yCoord > 0) {
-            if (!Character.isDigit(input.get(xCoord).charAt(yCoord-1))) {
-                surroundingSymbols.append(input.get(xCoord).charAt(yCoord - 1));
+        char symbol;
+        if (yCoordMin > 0) {
+            symbol = input.get(xCoord).charAt(yCoordMin-1);
+            if (symbol == '*') {
+                List<Integer> partNumbers = gears.get(new Point(xCoord, yCoordMin-1));
+                if (partNumbers == null) {
+                    partNumbers = new ArrayList<>();
+                }
+                partNumbers.add(partNumber);
+                gears.put(new Point(xCoord,yCoordMin-1), partNumbers);
             }
+
+            surroundingSymbols.append(symbol);
         }
 
-        if (yCoord < inputWidth-1) {
-            if (!Character.isDigit(input.get(xCoord).charAt(yCoord+1))) {
-                surroundingSymbols.append(input.get(xCoord).charAt(yCoord + 1));
+        if (yCoordMax < inputWidth-1) {
+            symbol = input.get(xCoord).charAt(yCoordMax+1);
+            if (symbol == '*') {
+                List<Integer> partNumbers = gears.get(new Point(xCoord, yCoordMax+1));
+                if (partNumbers == null) {
+                    partNumbers = new ArrayList<>();
+                }
+                partNumbers.add(partNumber);
+                gears.put(new Point(xCoord,yCoordMax+1), partNumbers);
             }
+
+            surroundingSymbols.append(symbol);
         }
 
+        //Line above xCoord
         if (xCoord > 0) {
-            for (int offset=-1; offset < 2; offset++) {
-                if (yCoord+offset >= 0 && yCoord+offset < inputWidth) {
-                    if (!Character.isDigit(input.get(xCoord - 1).charAt(yCoord + offset))) {
-                        surroundingSymbols.append(input.get(xCoord - 1).charAt(yCoord + offset));
+            for (int yCoord=yCoordMin-1; yCoord < yCoordMax+2; yCoord++) {
+                if (yCoord >= 0 && yCoord < inputWidth) {
+                    symbol = input.get(xCoord-1).charAt(yCoord);
+                    if (symbol == '*') {
+                        List<Integer> partNumbers = gears.get(new Point(xCoord-1, yCoord));
+                        if (partNumbers == null) {
+                            partNumbers = new ArrayList<>();
+                        }
+                        partNumbers.add(partNumber);
+                        gears.put(new Point(xCoord-1,yCoord), partNumbers);
                     }
+                    surroundingSymbols.append(symbol);
                 }
             }
         }
 
+        //Line below xCoord
         if (xCoord < inputHeight-1) {
-            for (int offset=-1; offset < 2; offset++) {
-                if (yCoord+offset >= 0 && yCoord+offset < inputWidth) {
-                    if (!Character.isDigit(input.get(xCoord + 1).charAt(yCoord + offset))) {
-                        surroundingSymbols.append(input.get(xCoord + 1).charAt(yCoord + offset));
+            for (int yCoord=yCoordMin-1; yCoord < yCoordMax+2; yCoord++) {
+                if (yCoord >= 0 && yCoord < inputWidth) {
+                    symbol = input.get(xCoord+1).charAt(yCoord);
+                    if (symbol == '*') {
+                        List<Integer> partNumbers = gears.get(new Point(xCoord+1, yCoord));
+                        if (partNumbers == null) {
+                            partNumbers = new ArrayList<>();
+                        }
+                        partNumbers.add(partNumber);
+                        gears.put(new Point(xCoord+1,yCoord), partNumbers);
                     }
+                    surroundingSymbols.append(input.get(xCoord+1).charAt(yCoord));
                 }
             }
         }
+
         return surroundingSymbols.toString();
     }
 
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -10,40 +10,23 @@ public class Day3Test {
 
     @Test
     public void day3Part1() {
-        System.out.println(day3.part1());
+        Assertions.assertEquals(535235, day3.part1());
     }
 
     @Test
     public void day3Part1Test() {
-        List<String> input = new ArrayList<>();
-        input.add("467..114..");
-        input.add("...*......");
-        input.add("..35..633.");
-        input.add("......#...");
-        input.add("617*......");
-        input.add(".....+.58.");
-        input.add("..592.....");
-        input.add("......755.");
-        input.add("...$.*....");
-        input.add(".664.598..");
-
-        Day3 day3Test = new Day3(input);
-        System.out.println(day3Test.part1());
+        Day3 day3Test = new Day3("src/main/Day3Test");
+        Assertions.assertEquals(4361, day3Test.part1());
     }
 
     @Test
-    public void validateThatSurroundingSymbolsAreCorrect() {
-        List<String> input = new ArrayList<>();
-        input.add("abc");
-        input.add("d9e");
-        input.add("fgh");
+    public void day3Part2() {
+        System.out.println(day3.part2());
+    }
 
-        Day3 day3Test = new Day3(input);
-
-        Assertions.assertEquals("bd", day3Test.getSurroundingSymbols(0, 0));
-        Assertions.assertEquals("be", day3Test.getSurroundingSymbols(0, 2));
-        Assertions.assertEquals("deabcfgh", day3Test.getSurroundingSymbols(1,1));
-        Assertions.assertEquals("ge", day3Test.getSurroundingSymbols(2,2));
-        Assertions.assertEquals("gd", day3Test.getSurroundingSymbols(2,0));
+    @Test
+    public void day3Part2Test() {
+        Day3 day3Test = new Day3("src/main/Day3Test");
+        Assertions.assertEquals(467835, day3Test.part2());
     }
 }
