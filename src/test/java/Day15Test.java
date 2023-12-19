3ba75b6f39f199cf83fc21864ff2128ac1850511
VOB <vobc@live.com>
2023-12-19 14:06:19 +0100

    day15 correct tests and test files

--- src/test/java/Day15Test.java
+++ src/test/java/Day15Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day15Test {
@@ -6,11 +7,23 @@ public class Day15Test {
 
     @Test
     public void Day15Part1() {
-        System.out.println(day15.part1());
+        Assertions.assertEquals(511343, day15.part1());
     }
 
     @Test
     public void Day15Part2() {
-        System.out.println(day15.part2());
+        Assertions.assertEquals(294474, day15.part2());
+    }
+
+    @Test
+    public void Day15Part1Test() {
+        Day15 day15Test = new Day15("src/main/Day15Test");
+        Assertions.assertEquals(1320, day15Test.part1());
+    }
+
+    @Test
+    public void Day15Part2Test() {
+        Day15 day15Test = new Day15("src/main/Day15Test");
+        Assertions.assertEquals(145, day15Test.part2());
     }
 }
