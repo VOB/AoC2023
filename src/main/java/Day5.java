a8754ca6f533cfb26b29cd476bf10a0b0c9c40c7
VOB <vobc@live.com>
2023-12-29 23:11:25 +0100

    day5part2 solution. heavy refactoring and bug fix

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,16 +8,23 @@ public class Day5 {
     TextParser textParser = new TextParser();
     List<List<String>> input;
 
+    List<Long> seeds;
+    List<List<Mapping>> blocks;
+
+
     public Day5() {
         input = textParser.blocks("src/main/Day5");
+        seeds = extractSeeds();
+        blocks = extractMappings();
     }
 
     public Day5(String input) {
         this.input = textParser.blocks(input);
+        seeds = extractSeeds();
+        blocks = extractMappings();
     }
 
     public long part1() {
-        List<Long> seeds = extractSeeds();
         long lowestLocation = 0;
         for (long seed : seeds) {
             long locationNumber = getLocationNumber(seed);
@@ -30,96 +38,63 @@ public class Day5 {
     }
 
     public long part2() {
-        List<Long> seeds = extractSeeds();
+        List<Long> seedsWithRanges = extractSeeds();
+        List<SeedRange> seedRanges = createSeedRanges(seedsWithRanges);
 
-        long lowestLocation = 0;
-        for (int i = 0; i < seeds.size(); i+=2) {
-            long seed = seeds.get(i);
-            long range = seeds.get(i+1);
-            System.out.println("getting lowest for " + seed + " with range " + range);
-            long lowestLocationForRange = getLowestLocationNumberForRange(seed, range);
-            if (lowestLocation == 0) {
-                lowestLocation = lowestLocationForRange;
-            } else if (lowestLocationForRange < lowestLocation) {
-                lowestLocation = lowestLocationForRange;
-            }
+        long locationIterator = 0;
+        while (!seedMappingExistsForLocation(locationIterator, seedRanges)) {
+            locationIterator++;
         }
-        return lowestLocation;
+        return locationIterator;
     }
 
-    public long getLowestLocationNumberForRange(long seed, long range) {
-        long lowestLocation = getLocationNumber(seed);
+    public boolean seedMappingExistsForLocation(long location, List<SeedRange> seedRanges) {
 
-        //List<Long> bounds = findNextMapRangeBounds(seed, 1);
-        //System.out.println("Bounds: " + bounds.get(0) + " " + bounds.get(1));
-        long iterator = 1;
-        int percentage = 0;
-        while (iterator < range) {
-            long locationNumber = getLocationNumber(seed+iterator);
-            int currentP = (int) ((double) iterator / range * 100);
-            if (currentP > percentage) {
-                percentage = currentP;
-                System.out.println(percentage);
-            }
-
-            if (locationNumber < lowestLocation) {
-                lowestLocation = locationNumber;
-
-                System.out.println("seed+iterator: " + (seed+iterator) + " location: " + locationNumber);
-            }
-            iterator++;
+        for (int i = blocks.size()-1; i>=0; i--) {
+            location = findPreviousMap(location, i);
         }
-        return lowestLocation;
+
+        for (SeedRange seedRange : seedRanges) {
+            if (location >= seedRange.start && location <= seedRange.end) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public long getLocationNumber(long seed) {
-        for (int i = 1; i < input.size(); i++) {
+        for (int i = 0; i < blocks.size(); i++) {
             seed = findNextMap(seed, i);
         }
         return seed;
     }
 
-    public List<Long> findNextMapRangeBounds(long previous, int nextBlock) {
-        List<String> block = input.get(nextBlock).subList(1,input.get(nextBlock).size());
+    public long findNextMap(long previous, int blockIndex) {
 
-        long lowerBound = 0;
-        long upperBound = Long.MAX_VALUE;
+        List<Mapping> block = blocks.get(blockIndex);
 
-        for (String mapping : block) {
-            List<Long> values = extractFields(mapping);
-            long sourceRangeStart = values.get(1);
-            long rangeLength = values.get(2);
-
-            if (previous > sourceRangeStart && sourceRangeStart > lowerBound) {
-                lowerBound = sourceRangeStart;
-            }
-
-            if (previous < sourceRangeStart+rangeLength && sourceRangeStart < upperBound) {
-                upperBound = sourceRangeStart+rangeLength;
+        for (Mapping mapping : block) {
+            if (mapping.sourceRangeStart <= previous && previous < (mapping.sourceRangeStart + mapping.rangeLength)) {
+                return mapping.destinationRangeStart + (previous - mapping.sourceRangeStart);
             }
         }
-
-        return Arrays.asList(lowerBound, upperBound);
-    }
-
-    public long findNextMap(long previous, int nextBlock) {
-
-        List<String> block = input.get(nextBlock).subList(1,input.get(nextBlock).size());
-
-        for (String mapping : block) {
-            List<Long> values = extractFields(mapping);
-            long destinationRangeStart = values.get(0);
-            long sourceRangeStart = values.get(1);
-            long rangeLength = values.get(2);
-
-            if (sourceRangeStart <= previous && previous <= (sourceRangeStart + rangeLength)) {
-                return destinationRangeStart + (previous - sourceRangeStart);
-            }
-        }
-
 
         return previous;
     }
+
+    public long findPreviousMap(long previous, int blockIndex) {
+
+        List<Mapping> block = blocks.get(blockIndex);
+
+        for (Mapping mapping : block) {
+            if (mapping.destinationRangeStart <= previous && previous < (mapping.destinationRangeStart + mapping.rangeLength)) {
+                return mapping.sourceRangeStart + (previous - mapping.destinationRangeStart);
+            }
+        }
+
+        return previous;
+    }
+
 
     public List<Long> extractSeeds() {
         List<String> seeds = Arrays.asList(input.get(0).get(0).split(": ")[1].split(" "));
@@ -129,7 +104,57 @@ public class Day5 {
                 .collect(Collectors.toList());
     }
 
-    private List<Long> extractFields(String mapping) {
-        return Arrays.stream(mapping.split(" ")).map(Long::parseLong).collect(Collectors.toList());
+    private List<List<Mapping>> extractMappings() {
+        List<List<Mapping>> mappingsList = new ArrayList<>();
+
+        for (int i=1; i<input.size(); i++) {
+            List<Mapping> mappings = new ArrayList<>();
+            List<String> block = input.get(i).subList(1,input.get(i).size());
+
+            for (String mapping : block) {
+                mappings.add(extractMapping(mapping));
+            }
+
+            mappingsList.add(mappings);
+        }
+
+        return mappingsList;
+    }
+
+    private Mapping extractMapping(String mapping) {
+        List<Long> mappingsAsLong = Arrays.stream(mapping.split(" ")).map(Long::parseLong).toList();
+        return new Mapping(mappingsAsLong.get(0), mappingsAsLong.get(1), mappingsAsLong.get(2));
+    }
+
+    private List<SeedRange> createSeedRanges(List<Long> seedsWithRanges) {
+        List<SeedRange> seedRanges = new ArrayList<>();
+
+        for (int i=0; i < seedsWithRanges.size(); i+=2) {
+            seedRanges.add(new SeedRange(seedsWithRanges.get(i), seedsWithRanges.get(i) + seedsWithRanges.get(i+1)));
+        }
+
+        return seedRanges;
+    }
+    private static class SeedRange {
+
+        long start;
+        long end;
+
+        public SeedRange(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    private static class Mapping {
+        long destinationRangeStart;
+        long sourceRangeStart;
+        long rangeLength;
+
+        public Mapping(long destinationRangeStart, long sourceRangeStart, long rangeLength) {
+            this.destinationRangeStart = destinationRangeStart;
+            this.sourceRangeStart = sourceRangeStart;
+            this.rangeLength = rangeLength;
+        }
     }
 }
--- src/test/java/Day5Test.java
+++ src/test/java/Day5Test.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigInteger;
-
 public class Day5Test {
 
     Day5 day5 = new Day5();
@@ -12,33 +10,21 @@ public class Day5Test {
         Assertions.assertEquals(525792406, day5.part1());
     }
 
-    @Test
-    public void verifyFirstSeedGetsItsLowestLocationTest() {
-        Day5 day5Test = new Day5("src/main/Day5Test");
-        Assertions.assertEquals(82, day5Test.getLocationNumber(day5Test.extractSeeds().get(0)));
-    }
 
-    //TODO result is wrong. code needs rework
-    /* @Test
+    @Test
     public void day5Part2() {
-        System.out.println(day5.part2());
-    } */
-
-    @Test
-    public void day5Part2Test() {
-        Day5 day5Test = new Day5("src/main/Day5Test");
-        Assertions.assertEquals(46, day5Test.part2());
+        Assertions.assertEquals(79004094, day5.part2());
     }
 
     @Test
-    public void verifyLowestLocationNumberInTest() {
+    public void day5Part1Test() {
         Day5 day5Test = new Day5("src/main/Day5Test");
         Assertions.assertEquals(35, day5Test.part1());
     }
 
     @Test
-    public void verifyLowestLocationForRangePair() {
+    public void day5Part2Test() {
         Day5 day5Test = new Day5("src/main/Day5Test");
-        Assertions.assertEquals(46, day5Test.getLowestLocationNumberForRange(79, 14));
+        Assertions.assertEquals(46, day5Test.part2());
     }
 }
