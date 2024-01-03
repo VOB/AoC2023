09c6a53a11e156a98241d23b18353f6537362df5
VOB <vobc@live.com>
2024-01-03 21:00:04 +0100

    day18part1solution

--- src/main/java/Day18.java
+++ src/main/java/Day18.java
@@ -1,19 +1,103 @@
+import java.awt.*;
+import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day18 {
 
     TextParser textParser = new TextParser();
-    List<List<String>> input;
+    Set<Edge> edgeSet;
+
+    Point U = new Point(-1,0);
+    Point R = new Point(0,1);
+    Point D = new Point(1,0);
+    Point L = new Point(0,-1);
 
     public Day18() {
-        input = textParser.blocks("src/main/Day18");
+        edgeSet = generateTrench(textParser.lines("src/main/Day18"));
+    }
+
+    public Day18(String path) {
+        edgeSet = generateTrench(textParser.lines(path));
     }
 
     public int part1() {
-        return 0;
+        Set<Point> lagoonCoords = edgeSet.stream().map(x->x.location).collect(Collectors.toSet());
+        for (Edge edge : edgeSet) {
+            switch (edge.direction) {
+                case "U" -> fillLagoon(edge.location, R, lagoonCoords);
+                case "R" -> fillLagoon(edge.location, D, lagoonCoords);
+                case "D" -> fillLagoon(edge.location, L, lagoonCoords);
+                case "L" -> fillLagoon(edge.location, U, lagoonCoords);
+            }
+        }
+        return lagoonCoords.size();
+    }
+
+    public void fillLagoon(Point point, Point direction, Set<Point> lagoon) {
+        Point maybeLagoonCoord = new Point(point.x+direction.x, point.y+direction.y);
+        lagoon.add(maybeLagoonCoord);
+        if (!edgeSet.contains(new Edge(maybeLagoonCoord))) {
+            fillLagoon(maybeLagoonCoord, direction, lagoon);
+        }
     }
 
     public int part2() {
         return 0;
+    }
+
+    public Set<Edge> generateTrench(List<String> lines) {
+        Set<Edge> edges = new HashSet<>();
+        Point currentLocation = new Point(0,0);
+
+        for (String line : lines) {
+            String direction = line.split(" ")[0];
+            int steps = Integer.parseInt(line.split(" ")[1]);
+            String hexColor = line.split(" ")[2].replaceAll("[#()]","");
+            for (int i=0; i<steps; i++) {
+                switch (direction) {
+                    case "U" -> currentLocation.setLocation(currentLocation.x - 1, currentLocation.y);
+                    case "R" -> currentLocation.setLocation(currentLocation.x, currentLocation.y + 1);
+                    case "D" -> currentLocation.setLocation(currentLocation.x + 1, currentLocation.y);
+                    case "L" -> currentLocation.setLocation(currentLocation.x, currentLocation.y - 1);
+                }
+                Edge newEdge = new Edge(new Point(currentLocation), direction, hexColor);
+                edges.add(newEdge);
+            }
+        }
+
+
+        return edges;
+    }
+
+    public static class Edge {
+        Point location;
+        String direction;
+        String hexColor;
+
+        public Edge(Point location, String direction, String hexColor) {
+            this.location = location;
+            this.direction = direction;
+            this.hexColor = hexColor;
+        }
+
+        public Edge(Point location) {
+            this.location = location;
+            direction = "";
+            hexColor = "";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Edge edge = (Edge) o;
+            return Objects.equals(location, edge.location);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(location);
+        }
     }
 }
--- src/test/java/Day18Test.java
+++ src/test/java/Day18Test.java
@@ -1,16 +1,23 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day18Test {
 
     Day18 day18 = new Day18();
 
-    //@Test
+    @Test
     public void Day18Part1() {
-        System.out.println(day18.part1());
+       Assertions.assertEquals(45159, day18.part1());
     }
 
     //@Test
     public void Day18Part2() {
         System.out.println(day18.part2());
+    }
+
+    @Test
+    public void Day18Part1Test() {
+        Day18 day18Test = new Day18("src/main/Day18Test");
+        Assertions.assertEquals(62, day18Test.part1());
     }
 }
